ce8258c46ce10ed0dbf4cc7324708687bf2b2cca
Elyas Amini <elyas.amini.fs@gmail.com>
2023-08-08 12:33:30 +0330

    Added password hash

---
+++ src/CSharp/EasyMicroservices.AuthenticationsMicroservice.Logics/Helpers/AuthenticationHelper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace EasyMicroservices.AuthenticationsMicroservice.Helpers
+{
+    public class AuthenticationHelper
+    {
+
+        public static async Task<string> HashPassword(string password)
+        {
+            var crypt = new SHA256Managed();
+            var hash = new StringBuilder();
+            byte[] crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(password));
+            foreach (byte theByte in crypto)
+            {
+                hash.Append(theByte.ToString("x2"));
+            }
+            return hash.ToString();
+        }
+    }
+}
