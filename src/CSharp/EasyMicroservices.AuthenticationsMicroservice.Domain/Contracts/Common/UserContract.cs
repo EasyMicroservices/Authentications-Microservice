e66058df4b4146f2cc95a221c1ceb7710db80fef
Mahdiyar Ghannad <m.gh.1385.mahdiyar@gmail.com>
2023-08-12 11:29:00 +0330

    Support for new upgrade of the core package #11

--- src/CSharp/EasyMicroservices.AuthenticationsMicroservice.Domain/Contracts/Common/UserContract.cs
+++ src/CSharp/EasyMicroservices.AuthenticationsMicroservice.Domain/Contracts/Common/UserContract.cs
@@ -7,11 +7,15 @@ using EasyMicroservices.Cores.Interfaces;
 
 namespace EasyMicroservices.AuthenticationsMicroservice.Contracts.Common
 {
-    public class UserContract : IUniqueIdentitySchema
+    public class UserContract : IUniqueIdentitySchema, ISoftDeleteSchema, IDateTimeSchema
     {
         public long Id { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
         public string UniqueIdentity { get; set; }
+        public DateTime CreationDateTime { get; set; }
+        public DateTime? ModificationDateTime { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime? DeletedDateTime { get; set; }
     }
 }
